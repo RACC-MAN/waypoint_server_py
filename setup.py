2f796304160765d48e79c7305e5a8e87830b04b6
RACC-MAN <130726633+RACC-MAN@users.noreply.github.com>
2025-01-25 11:47:48 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'waypoint_server = waypoint_server_py.waypoint_server:main',
         ],
     },
 )
